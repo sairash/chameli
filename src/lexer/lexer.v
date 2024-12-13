1b382c730bad979fe8b1cb49c52b1b5dbf777232
Sai On Action <gautamsairash@gmail.com>
2024-12-14 02:22:33 +0545

    lex struct created

--- src/lexer/lexer.v
+++ src/lexer/lexer.v
@@ -1,3 +1,21 @@
 module lexer
 
 import token
+
+
+pub struct Lex {
+	pub:
+		x int
+		cur_line int
+		cur_col int
+		file_data string
+		file_len int
+		next_token []token.Token
+		cur_token token.Token
+		before_consume_token []token.Token
+		consumed_token []token.Token
+}
+
+pub fn Lex.new() Lex {
+	return Lex{}
+}
