09717c1386b814a1fd57f53952a42af9a7bbce86
Sai On Action <gautamsairash@gmail.com>
2024-12-22 00:58:35 +0545

    removed error from main

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"chameli/chameli_error"
 	"chameli/lexer"
 	"fmt"
 )
 
 func main() {
-
-	x := chameli_error.Error{
-		Path:      "./test/error/error_test.gm",
-		CurLine:   9,
-		CurCol:    0,
-		Range:     []int{16, 20},
-		CodeError: true,
-		Error:     chameli_error.ErrorFileIO{FilePath: "./test/error/error_test.gm"},
-	}
-
-	x.ErrorGen()
 
 	lex := lexer.New("./test/lex/lua.gm")
 	fmt.Println(lex)
