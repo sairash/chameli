44c2a5cf82522b3198328b86ee3919bdaca5e3a9
Sai On Action <gautamsairash@gmail.com>
2024-12-24 01:12:50 +0545

    initiaiton of lexers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -3,6 +3,7 @@ package lexer
 import (
 	"chameli/token"
 	"os"
+	"unicode"
 )
 
 type Lex struct {
@@ -15,6 +16,64 @@ type Lex struct {
 	CurToken           token.Token
 	BeforeConsumeToken []token.Token
 	ConsumedToken      []token.Token
+}
+
+func (l *Lex) Next() token.Token {
+	l.SkipWhiteSpace()
+
+	next, eof := l.consume()
+
+	if eof {
+		return token.EOFTOKEN.AddRange([2]int{l.X, l.X})
+	}
+
+	return l.Matcher(next)
+
+}
+
+func (l *Lex) Matcher(next_char byte) token.Token {
+	switch next_char {
+	case '\n':
+		return token.EOLTOKEN.AddRange([2]int{l.X, l.X})
+
+	}
+	return token.EOLTOKEN.AddRange([2]int{l.X, l.X})
+}
+
+func (l *Lex) SkipWhiteSpace() {
+	for {
+		data, eof := l.peek()
+		if eof || data == '\n' || !unicode.IsSpace(rune(data)) {
+			break
+		}
+
+		l.X += 1
+		l.CurCol += 1
+	}
+
+}
+
+func (l *Lex) peek() (byte, bool) {
+	if l.X >= l.FileLen {
+		return 0, true
+	}
+
+	return l.FileData[l.X], false
+}
+
+func (l *Lex) consume() (byte, bool) {
+	next, eof := l.peek()
+	if eof {
+		return 0, true
+	}
+	if next == '\n' {
+		l.CurCol = 0
+		l.CurLine += 1
+	}
+
+	l.X += 1
+	l.CurCol += 1
+	return next, false
 }
 
 func GoThroughFile(path string) []byte {
