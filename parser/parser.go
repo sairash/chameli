c544c4b821288a8f82d77206b875c33ae64dfc04
Sai On Action <gautamsairash@gmail.com>
2024-12-27 02:16:33 +0545

    parser updated

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"chameli/chameli_error"
 	"chameli/lexer"
 	"chameli/token"
 	"fmt"
@@ -8,27 +9,58 @@ import (
 
 type Parser struct {
 	Lex                *lexer.Lex
-	NextToken          []*token.Token
+	NextToken          *token.Token
 	CurToken           *token.Token
 	BeforeConsumeToken []*token.Token
 }
 
-func (p *Parser) next() {
-	p.CurToken = p.NextToken[0]
+func (p *Parser) next() *chameli_error.Error {
+	tok, err := p.Lex.Next()
+	if err != nil {
+		return err
+	}
+
+	if p.CurToken.Value != "" {
+		p.BeforeConsumeToken = append(p.BeforeConsumeToken, p.CurToken)
+	}
+
+	p.CurToken = p.NextToken
+
+	p.NextToken = tok
+
+	return nil
+}
+
+func (p *Parser) errorGenerator(from string, error_data chameli_error.ErrorInterface) *chameli_error.Error {
+	fmt.Println(p.CurToken)
+	return &chameli_error.Error{
+		Path:      p.Lex.Path,
+		CurLine:   p.Lex.CurLine,
+		CurCol:    p.Lex.CurCol,
+		Range:     p.CurToken.TokenRange,
+		CodeError: true,
+		From:      from,
+		Error:     error_data,
+	}
 
 }
 
-func (p *Parser) Walk() {
+func (p *Parser) Walk() *chameli_error.Error {
 	for {
-		tok, err := p.Lex.Next()
+		err := p.next()
 
-		fmt.Println(tok)
 		if err != nil {
-			err.ErrorGen()
-			break
+			return err
 		}
-		if tok.TokenType == token.EOF {
-			break
+
+		if p.CurToken.Value == "" {
+			continue
+		}
+		switch p.CurToken.TokenType {
+		case token.EOF:
+			return nil
+		default:
+			return p.errorGenerator("parse walk", chameli_error.ErrorUnexpectedToken{Token: p.CurToken.Value})
 		}
 	}
 }
@@ -36,7 +68,8 @@ func (p *Parser) Walk() {
 func New(lex lexer.Lex) *Parser {
 	return &Parser{
 		Lex:                &lex,
-		NextToken:          []*token.Token{},
+		NextToken:          &token.Token{},
 		BeforeConsumeToken: []*token.Token{},
+		CurToken:           &token.Token{},
 	}
 }
