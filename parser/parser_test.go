00e08aeba5d6951d3b2897c8a41f13c166934ff5
Sai On Action <gautamsairash@gmail.com>
2025-06-04 01:57:03 +0545

    updated tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,14 +2,34 @@ package parser
 
 import (
 	"chameli/lexer"
+	"fmt"
 	"testing"
 )
 
 func TestWalk(t *testing.T) {
 	parse := New(lexer.New("../test/parse/walk.gm"))
-	err := parse.Walk()
+	parseTree, err := parse.Walk()
 	if err != nil {
 		t.Fatal(err.Error.Output())
 	}
 
+	for parse := range parseTree {
+		fmt.Println(parse)
+	}
+}
+
+func TestParseIdentifier(t *testing.T) {
+	parse := New(lexer.New("../test/parse/identifier.gm"))
+
+	parseTree, err := parse.Walk()
+
+	if err != nil {
+		t.Fatal(err.Error.Output())
+	}
+
+	expected := 3
+
+	if len(parseTree) != expected {
+		t.Fatal("Expected Amount:", expected, "Got Amount:", len(parseTree))
+	}
 }
