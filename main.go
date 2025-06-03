d98303921b6f0b131c5326ef2c31f393a32ae121
Sai On Action <gautamsairash@gmail.com>
2025-06-04 01:56:04 +0545

    updated main

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"chameli/lexer"
+	"chameli/parser"
 	"fmt"
 )
 
 func main() {
+	parse := parser.New(lexer.New("./test/parse/identifier.gm"))
+	parseTree, err := parse.Walk()
+	if err != nil {
+		err.ErrorGen()
+		return
+	}
 
-	lex := lexer.New("./test/lex/lua.gm")
-	fmt.Println(lex)
+	for _, v := range parseTree {
+		fmt.Println(v)
+	}
 }
